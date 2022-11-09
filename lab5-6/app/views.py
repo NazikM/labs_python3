019086c85eb2a198015297d0bcaaa2c79016a9a4
Nazar Marusyn <marusinnazar@gmail.com>
2022-11-09 15:52:11 +0200

    Update lab6

--- lab5-6/app/views.py
+++ lab5-6/app/views.py
@@ -27,45 +27,43 @@ def about():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = Myform()
-    if request.method == 'GET':
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        minute = now.strftime("%M")
-        return render_template('contact.html', session=session, form=form, time=current_time, minute=minute, page=3)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            username = form.name.data
+            email = form.email.data
+            phone = form.phone.data
+            subject = form.subject.data
+            message = form.message.data
+            session['name'] = username
+            session['email'] = email
 
-    if form.validate_on_submit():
-        username = form.name.data
-        email = form.email.data
-        phone = form.phone.data
-        subject = form.subject.data
-        message = form.message.data
-        session['name'] = username
-        session['email'] = email
+            contact_info = ContactInfo(username=username, email=email, phone=phone, subject=subject, message=message)
+            db.session.add(contact_info)
+            db.session.commit()
 
-        contact_info = ContactInfo(username=username, email=email, phone=phone, subject=subject, message=message)
-        db.session.add(contact_info)
-        db.session.commit()
-
-        form.name.data = 'виконався метод post і валідація успішно'
-        flash(f"Дані успішно відправлено: {username} {email}", category='success')
+            form.name.data = 'виконався метод post і валідація успішно'
+            flash(f"Дані успішно відправлено: {username} {email}", category='success')
+            return redirect(url_for("contact"))
+        flash("Не пройшла валідація з Post", category='warning')
         return redirect(url_for("contact"))
-
-    flash("Не пройшла валідація з Post", category='warning')
-    return redirect(url_for("contact"))
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    minute = now.strftime("%M")
+    return render_template('contact.html', session=session, form=form, time=current_time, minute=minute, page=3)
+    
 
 
 @app.route('/contact_info', methods=['GET'])
 def contact_info():
-    user_contact_id = request.args.get('id')
-    if user_contact_id:
-        contact_info_list = ContactInfo.query.filter_by(id=user_contact_id)
-    else:
-        contact_info_list = ContactInfo.query.all()
+    contact_info_list = ContactInfo.query.all()
     return render_template('contact_info.html', contact_info_list=contact_info_list)
 
 
 @app.route('/delete_contact_info/<int:number>', methods=['GET'])
 def delete_contact_info(number):
-    ContactInfo.query.filter_by(id=number).delete()
-    db.session.commit()
+    try:
+        ContactInfo.query.filter_by(id=number).delete()
+    except:
+        db.session.commit()
+        db.session.rollback()
     return '<h1>Successfully deleted!</h1>'
